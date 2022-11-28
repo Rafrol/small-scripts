cf00b23581e9d08e8b7dbe5bbe5ecaf5b6445667
Ramon Oliveira <116411372+Rafrol@users.noreply.github.com>
2022-11-28 01:25:38 -0300

    Update fahrenheit-to-celsius-table.py
    After learning the .format() method in Crash Course on Python (Week 4)

--- fahrenheit-to-celsius-table.py
+++ fahrenheit-to-celsius-table.py
@@ -1,4 +1,4 @@
 def to_celsius(x):
   return (x-32)*5/9
 for x in range (0,131,5):
-  print(str(x) + "degrees in Fahrenheit is the same as " + str(to_celsius(x)) + " degrees in Celsius")
+  print("{:>3} F | {:>6.2f} C".format(x, to_celsius(x)))
