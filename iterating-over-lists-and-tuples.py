8f14519afba11e7d61e40933f930a7f17bd21d49
Ramon Oliveira <116411372+Rafrol@users.noreply.github.com>
2022-11-29 01:11:29 -0300

    Update iterating-over-lists-and-tuples.py

--- iterating-over-lists-and-tuples.py
+++ iterating-over-lists-and-tuples.py
@@ -1,5 +1,6 @@
-#Exercise extracted from Crash Courser on Python (Google/Cousera). 
-#I just found it interesting and am putting this code here.
+# Exercise extracted from Crash Courser on Python (Google/Cousera).
+# This exercise is inside the "Iterating over Lists and Tuples" video, at Week 4.
+# I just found it interesting and am putting this code here.
 
 '''Try out the enumerate function for yourself in this quick exercise. Complete the skip_elements function to return every other element from the list, this time using the enumerate function to check if an element is in an even position or an odd position.
 '''
